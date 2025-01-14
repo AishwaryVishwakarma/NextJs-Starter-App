02593c3fc156d4ef49cb7ebda9fce8a83090f639
AishwaryVishwakarma <aishwaryvishwakarma23@gmail.com>
2025-01-14 19:00:10 +0530

    init ⚡

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
+
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: IS_PRODUCTION,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
